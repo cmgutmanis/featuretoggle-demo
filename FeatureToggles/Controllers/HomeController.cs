930495c90a08346d07dd04e82940d62b4508a1c0
Chris Gutmanis <chris.gutmanis@gmail.com>
2015-11-16 08:02:43 -0600

    initial commit

---
+++ FeatureToggles/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using FeatureToggles.Controllers.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Toggles;
+
+namespace FeatureToggles.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var toggler = new SimpleConfigToggleManager();
+
+            var isEnabled = toggler.IsActive<HomeTestFeature>();
+
+            return View();
+        }
+    }
+}
---
+++ FeatureToggles/Toggles/SimpleConfigToggleManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Toggles
+{
+    public class SimpleConfigToggleManager : IFeatureToggle
+    {
+
+        public bool IsActive<TFeature>()
+        {
+            var key = typeof(TFeature).Name;
+
+            var result = false;
+            
+            bool.TryParse(ConfigurationManager.AppSettings[key], out result);
+
+            return result;
+        }
+    }
+}
